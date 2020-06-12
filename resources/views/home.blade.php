1203b150a64d49755618119bcd270cc25abca197
x7airworker <jake@jakes-co.de>
2020-06-12 22:03:19 +0200

    Fixed main stream

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Start')
 
 @section('content')
-    <iframe src="https://player.twitch.tv/?channel=Wubbl0rz&parent=localhost" width="100%" height="720">
+    <iframe src="https://player.twitch.tv/?channel=Wubbl0rz&parent={{ env('TWITCH_PARENT') }}" width="100%" height="720">
 
     </iframe>
 @endsection
