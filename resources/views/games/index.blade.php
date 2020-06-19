f901c415bf1a714b5b5ef2bc630b2fc1e01849f4
x7airworker <jake@jakes-co.de>
2020-06-19 12:18:39 +0200

    Flush list and prepared for streams

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -3,11 +3,13 @@
 @section('title', 'Spiele')
 
 @section('content')
-    <div class="list-group mb-3">
+    <div class="list-group mb-3 list-group-flush">
         @foreach ($games as $game)
             <a class="list-group-item list-group-item-action" href="{{ route('games.show', [$game->game]) }}">
                 <span class="badge badge-info">{{ $game->num }} Clips</span>
                 &nbsp;
+                <!-- <span class="badge badge-primary">0 Streams</span>
+                &nbsp; !-->
                 {{ $game->game }}
             </a>
         @endforeach
