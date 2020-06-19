ea577f62ab50de74135179a85a07a60612a9904a
x7airworker <jake@jakes-co.de>
2020-06-19 12:14:43 +0200

    Added some margin

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Spiele')
 
 @section('content')
-    <div class="list-group">
+    <div class="list-group mb-3">
         @foreach ($games as $game)
             <a class="list-group-item list-group-item-action" href="{{ route('games.show', [$game->game]) }}">
                 <span class="badge badge-info">{{ $game->num }} Clips</span>
